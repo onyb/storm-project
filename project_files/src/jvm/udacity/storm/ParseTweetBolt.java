a5c38ca79b956ca9d8d5d6cfbc7fe329dce46379
Anirudha Bose <ani07nov@gmail.com>
2015-01-24 20:16:08 +0530

    Do not emit parts of speech in output collector

--- project_files/src/jvm/udacity/storm/ParseTweetBolt.java
+++ project_files/src/jvm/udacity/storm/ParseTweetBolt.java
@@ -23,87 +23,56 @@ import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import udacity.storm.tools.CountiesLookup;
 
 /**
  * A bolt that parses the tweet into words
  */
+
 public class ParseTweetBolt extends BaseRichBolt
 {
-  // To output tuples from this bolt to the count bolt
-  OutputCollector collector;
-  StringBuilder result;
-  private String[] skipWords = {"rt", "to", "me","la","on","that","que",
-    "followers","watch","know","not","have","like","I'm","new","good","do",
-    "more","es","te","followers","Followers","las","you","and","de","my","is",
-    "en","una","in","for","this","go","en","all","no","don't","up","are",
-    "http","http:","https","https:","http://","https://","with","just","your",
-    "para","want","your","you're","really","video","it's","when","they","their","much",
-    "would","what","them","todo","FOLLOW","retweet","RETWEET","even","right","like",
-    "bien","Like","will","Will","pero","Pero","can't","were","Can't","Were","TWITTER",
-    "make","take","This","from","about","como","esta","follows","followed"};
+    OutputCollector collector;
+    StringBuilder result;
+    CountiesLookup clookup;
 
-  //MaxentTagger tagger ;
-  CountiesLookup clookup ;
-  
-  @Override
-  public void prepare(
-      Map                     map,
-      TopologyContext         topologyContext,
-      OutputCollector         outputCollector)
-  {
-    // save the output collector for emitting tuples
-    collector = outputCollector;
-    //tagger	= new MaxentTagger("models/english-caseless-left3words-distsim.tagger");
-    clookup= new CountiesLookup();
-
-  	 result = new StringBuilder();
-  }
-
-  @Override
-  public void execute(Tuple tuple)
-  {
-    // get the 1st column 'tweet' from tuple
-    String tweet = tuple.getStringByField("tweet").split("DELIMITER")[0];
-    double latitude = Double.parseDouble(tuple.getStringByField("tweet").split("DELIMITER")[1].split(",")[0]);
-    double longitude = Double.parseDouble(tuple.getStringByField("tweet").split("DELIMITER")[1].split(",")[1]);
-    String county_id = clookup.getCountyCodeByGeo(latitude, longitude);
-
-    int sentiment = tuple.getIntegerByField("sentiment");
-    
-    String url = tuple.getString(0).split("DELIMITER")[2];
-
-    
-    // provide the delimiters for splitting the tweet
-    String delims = "[ .,?!]+";
-
-    String [] posTweet = {"", "", ""};
-    //String [] posTweet = new String[]{"i" ,"got" ,"ebola"};
-    if(posTweet != null)
-    {    
-    	String[] tokens = tweet.split(delims);
-      	System.out.print("\tParseTweetBolt\tDEBUG:" + posTweet + ", URL: " + url + "\n");
-	    // for each token/word, emit it
-      	result.setLength(0);
-      	int n = tokens.length;
-	    for (int i = 0; i < n; i++) {
-	    	if(!Arrays.asList(skipWords).contains(tokens[i])){
-	    		result.append(tokens[i]); 
-	   	      }
-	    }
-	    
-	    collector.emit(new Values(tweet, result.toString(), posTweet[0], posTweet[1], posTweet[2], county_id, url, sentiment));
-
-		
+    @Override
+    public void prepare(
+        Map                     map,
+        TopologyContext         topologyContext,
+        OutputCollector         outputCollector)
+    {
+        collector = outputCollector;
+        clookup= new CountiesLookup();
+        result = new StringBuilder();
     }
-  }  
-  @Override
-  public void declareOutputFields(OutputFieldsDeclarer declarer)
-  {
-    // tell storm the schema of the output tuple for this spout
-    // tuple consists of a single column called 'tweet-word'
-    declarer.declare(new Fields("original-tweet", "tweet-word", "noun", "verb", "object", "county_id", "url", "sentiment"));
-  }
 
+    @Override
+    public void execute(Tuple tuple)
+    {
+        String tweet = tuple.getStringByField("tweet").split("DELIMITER")[0];
+
+        double latitude = Double.parseDouble(tuple.getStringByField("tweet")
+                                                  .split("DELIMITER")[1]
+                                                  .split(",")[0]);
+
+        double longitude = Double.parseDouble(tuple.getStringByField("tweet")
+                                                   .split("DELIMITER")[1]
+                                                   .split(",")[1]);
+
+        String county_id = clookup.getCountyCodeByGeo(latitude, longitude);
+
+        int sentiment = tuple.getIntegerByField("sentiment");
+
+        String url = tuple.getString(0).split("DELIMITER")[2];
+
+        collector.emit(new Values(tweet, county_id, url, sentiment));
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer)
+    {
+        /* Tell Storm the schema of the output tuple for this spout
+           tuple consists of a single column called 'tweet' */
+        declarer.declare(new Fields("original-tweet", "county_id", "url", "sentiment"));
+    }
 }
