7ef69c98da31abb8cd7e01c34749234f59fef9c4
Anirudha Bose <ani07nov@gmail.com>
2015-02-01 16:31:23 +0530

    Change class name of StormTopology.java

--- src/jvm/storm/twitter/StormTopology.java
+++ src/jvm/storm/twitter/StormTopology.java
@@ -7,7 +7,7 @@ import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
 import backtype.storm.utils.Utils;
 
-class TopNTweetTopology
+class StormTopology
 {
     public static void main(String[] args) throws Exception
     {
